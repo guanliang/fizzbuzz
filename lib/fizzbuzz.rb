5a862af11e4cd00112559fbc97d7d11a0e004fd9
Guanliang Liu <guanliang.liu@gmail.com>
2016-05-24 13:31:27 +0100

    implementation of stage 2 and a bit clean up

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,18 @@
 module Fizzbuzz
+  def fizzbuzz
+    return 'fizzbuzz' if self.is_divisible_by?(15)
+    return 'fizz' if self.is_divisible_by?(3) || self.contains?(3)
+    return 'buzz' if self.is_divisible_by?(5) || self.contains?(5)
+    self
+  end
+
   def is_divisible_by?(num)
     self % num == 0
   end
 
+  def contains?(digit)
+    self.to_s.include?(digit.to_s)
+  end
 end
 
 class Integer
@@ -11,10 +21,7 @@ end
 
 def run
   (1..100).to_a.each do |i|
-    puts 'fizzbuzz' if i.is_divisible_by?(15)
-    puts 'fizz' if i.is_divisible_by?(3)
-    puts 'buzz' if i.is_divisible_by?(5)
-    puts i
+    puts i.fizzbuzz
   end
 end
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,4 +13,42 @@ describe 'Fizzbuzz' do
       expect((3 * multiple).is_divisible_by?(11)).to be_falsey
     end
   end
+
+  context 'Integers will response to an extended method called contains? and return true or false accordingly' do
+    let(:digits) { (1..9).to_a }
+    let(:contained) { digits.sample(2) }
+    let(:integer) { contained.first * 10 + contained.last }
+
+    it 'returns true if an integer contains given digit' do
+      contained.each do |c|
+        expect(integer.contains?(c)).to be_truthy
+      end
+    end
+
+    it 'returns false if an integer does not contain given digit' do
+      (digits - contained).each do |c|
+        expect(integer.contains?(c)).to be_falsey
+      end
+    end
+  end
+
+  context 'Integer will response to an extended method called fizzbuzz and return the right output' do
+    it 'returns fizzbuzz if an integer can be divided by 15' do
+      expect(15.fizzbuzz).to eq 'fizzbuzz'
+    end
+
+    it 'returns fizz if an integer can be divided by 3' do
+      expect(3.fizzbuzz).to eq 'fizz'
+    end
+
+    it 'returns buzz if an integer can be divided by 5' do
+      expect(5.fizzbuzz).to eq 'buzz'
+    end
+
+    it 'returns itself if an integer can not be divided by either 3 or 5' do
+      expect(14.fizzbuzz).to eq 14
+    end
+
+
+  end
 end
